e111eb0363778589bc5bcf718a89efeb20c94912
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-29 14:31:41 -0700

    register a user test passing with controller, model and service in place

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,7 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS secrets;
+DROP TABLE IF EXISTS secrets CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -14,6 +14,5 @@ CREATE TABLE secrets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    created_at TIMESTAMP,
-    user_id BIGINT REFERENCES users(id)
+    created_at TIMESTAMP
 )
